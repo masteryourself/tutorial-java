bc3205ac33be54f13a7bfeaf8cf459b7faa55e4e
masteryourself <renzhao.ruan@foxmail.com>
2020-05-04 13:30:12 +0800

    CPUTest 案例

---
+++ tutorial-jvm/tutorial-gc/src/main/java/pers/masteryourself/tutorial/jvm/gc/CPUTest.java
@@ -0,0 +1,27 @@
+package pers.masteryourself.tutorial.jvm.gc;
+
+
+import java.util.Random;
+
+/**
+ * <p>description : CPUTest
+ *
+ * <p>blog : https://blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/5/3 20:09
+ */
+public class CPUTest {
+
+    public static void main(String[] args) {
+        while (true) {
+            int begin = new java.util.Random().nextInt(545775512);
+            for (int i = 0; i < 50; i++) {
+                begin = begin + new java.util.Random().nextInt(545775512);
+            }
+            System.out.println(begin);
+        }
+    }
+
+}
