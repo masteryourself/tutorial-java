857b3374a61bb821324df1ecd0ac8e10e82f8d3c
masteryourself <renzhao.ruan@foxmail.com>
2022-04-07 18:54:54 +0800

    ByteBuffer 案例

---
+++ tutorial-io/src/main/java/org/masteryourself/tutorial/nio/ByteBufferTest1.java
@@ -0,0 +1,48 @@
+package org.masteryourself.tutorial.nio;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Objects;
+
+/**
+ * <p>description : ByteBufferTest1
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/4/7 6:28 PM
+ */
+@Slf4j
+public class ByteBufferTest1 {
+
+    public static void main(String[] args) {
+        // 准备输入流
+        String fileName = Objects.requireNonNull(ByteBufferTest1.class.getClassLoader().getResource("data.txt")).getFile();
+        try (FileChannel channel = new FileInputStream(fileName).getChannel()) {
+            // 准备缓冲区
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+            while (true) {
+                int len = channel.read(buffer);
+                log.info("一共读到 {} 个字节数", len);
+                if (len == -1) {
+                    break;
+                }
+                // 切换到读模式
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    log.info("读取到内容 {}", (char) buffer.get());
+                }
+                // 切换到写模式
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
