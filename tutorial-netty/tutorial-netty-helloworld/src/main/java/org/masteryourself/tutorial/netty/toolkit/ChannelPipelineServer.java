98feb2702b95be48f043eb8a14e35113b383d286
masteryourself <renzhao.ruan@foxmail.com>
2022-04-14 23:56:15 +0800

    pipeline

---
+++ tutorial-netty/tutorial-netty-helloworld/src/main/java/org/masteryourself/tutorial/netty/toolkit/ChannelPipelineServer.java
@@ -0,0 +1,78 @@
+package org.masteryourself.tutorial.netty.toolkit;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>description : ChannelPipelineServer
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/4/11 4:34 PM
+ */
+@Slf4j
+public class ChannelPipelineServer {
+
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("inbound-1");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("inbound-2");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("inbound-3");
+                                // 响应客户端
+                                ctx.channel().writeAndFlush(msg);
+                            }
+                        });
+                        ch.pipeline().addLast(new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.info("outbound-4");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        ch.pipeline().addLast(new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.info("outbound-5");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        ch.pipeline().addLast(new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.info("outbound-6");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                    }
+                })
+                // 4. ServerSocketChannel 绑定的监听端口
+                .bind(9527);
+    }
+
+}
