15fee3f76316e861ad92c201310082ff8b6b2e93
masteryourself <renzhao.ruan@foxmail.com>
2022-04-15 17:55:14 +0800

    分隔符

---
+++ tutorial-netty/tutorial-netty-packet/src/main/java/org/masteryourself/tutorial/netty/linebased/LineBasedNettyServer.java
@@ -0,0 +1,51 @@
+package org.masteryourself.tutorial.netty.linebased;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>description : LineBasedNettyServer
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/4/15 5:49 PM
+ */
+@Slf4j
+public class LineBasedNettyServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boos = new NioEventLoopGroup(1);
+        NioEventLoopGroup work = new NioEventLoopGroup();
+        try {
+            ChannelFuture channelFuture = new ServerBootstrap()
+                    .group(boos, work)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        }
+                    })
+                    .bind(9527);
+            channelFuture.sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            boos.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+    }
+
+}
