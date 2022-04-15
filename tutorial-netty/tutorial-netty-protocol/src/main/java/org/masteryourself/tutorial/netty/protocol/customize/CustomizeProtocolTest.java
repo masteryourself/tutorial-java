02fc1e70fe42cdc6603a3bcd0760ce1abdd4ebbe
masteryourself <renzhao.ruan@foxmail.com>
2022-04-15 20:40:38 +0800

    自定义协议

---
+++ tutorial-netty/tutorial-netty-protocol/src/main/java/org/masteryourself/tutorial/netty/protocol/customize/CustomizeProtocolTest.java
@@ -0,0 +1,37 @@
+package org.masteryourself.tutorial.netty.protocol.customize;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * <p>description : CustomizeProtocolTest
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/4/15 8:28 PM
+ */
+public class CustomizeProtocolTest {
+
+    public static void main(String[] args) throws Exception {
+        EmbeddedChannel channel = new EmbeddedChannel(
+                new LoggingHandler(),
+                new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
+                new MessageCodec()
+        );
+        Message message = Message.builder()
+                .messageType((byte) 1)
+                .sequenceId(10001)
+                .username("zhangsan")
+                .password("123456")
+                .build();
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
+        new MessageCodec().encode(null, message, buf);
+        channel.writeInbound(buf);
+    }
+
+}
---
+++ tutorial-netty/tutorial-netty-protocol/src/main/java/org/masteryourself/tutorial/netty/protocol/customize/Message.java
@@ -0,0 +1,29 @@
+package org.masteryourself.tutorial.netty.protocol.customize;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * <p>description : Message
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/4/15 8:13 PM
+ */
+@Data
+@Builder
+public class Message implements Serializable {
+
+    private Byte messageType;
+
+    private Integer sequenceId;
+
+    private String username;
+
+    private String password;
+
+}
