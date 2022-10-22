398d62e983e153ddbe3e6a97f437f0d1d9ac95bc
masteryourself <renzhao.ruan@foxmail.com>
2022-10-22 19:15:01 +0800

    线程池处理异常

---
+++ tutorial-concurrent/src/main/java/org/masteryourself/tutorial/concurrent/threadpool/exception/ThreadPoolExceptionHandle.java
@@ -0,0 +1,64 @@
+package org.masteryourself.tutorial.concurrent.threadpool.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * <p>description : ThreadPoolExceptionHandle
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/10/22 18:57
+ */
+@Slf4j
+public class ThreadPoolExceptionHandle {
+
+    public static void main(String[] args) throws Exception {
+        exceptionHandle1();
+        exceptionHandle2();
+        exceptionHandle3();
+    }
+
+    private static void exceptionHandle1() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        threadPool.execute(() -> {
+            try {
+                int i = 10 / 0;
+            } catch (Exception e) {
+                log.error("线程池运行异常", e);
+            }
+            log.info("任务运行结束");
+        });
+    }
+
+    private static void exceptionHandle2() throws Exception {
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        Future<?> future = threadPool.submit(() -> {
+            int i = 10 / 0;
+            log.info("任务运行结束");
+        });
+        log.info("res {}", future.get());
+    }
+
+    private static void exceptionHandle3() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(2, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setUncaughtExceptionHandler((t, e) -> log.info("线程池运行异常", e));
+                return thread;
+            }
+        });
+        threadPool.execute(() -> {
+            int i = 10 / 0;
+            log.info("任务运行结束");
+        });
+    }
+
+}
