6fb349018a56804c1795718b0ff414ae27d39be6
masteryourself <renzhao.ruan@foxmail.com>
2022-12-09 15:59:03 +0800

    二分查找法和冒泡排序

---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/BubbleEnhanceSort.java
@@ -0,0 +1,54 @@
+package org.masteryourself.tutorial.algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * <p>description : BubbleEnhanceSort
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/12/9 15:54
+ */
+@Slf4j
+public class BubbleEnhanceSort {
+
+    public static void bubbleEnhance(int[] array) {
+        AtomicInteger cycleCount = new AtomicInteger();
+        // 记录上一次冒泡的交换索引
+        int nextCycleCount = array.length - 1;
+        // 一共需要交换 array.length - 1 轮
+        while (true) {
+            // 每一轮冒泡是为了找出最大值, 放到最右边
+            int n = 0;
+            for (int i = 0; i < nextCycleCount; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                    n = i;
+                }
+            }
+            nextCycleCount = n;
+            log.info("第 {} 轮冒泡, 循环次数是 {}, 冒泡结果是 {}", cycleCount.incrementAndGet(), nextCycleCount, array);
+            // n=0 表示上轮排序已经没有发生交换, 不再需要冒泡了
+            if (n == 0) {
+                log.info("数组已经有序了, 不需要再冒泡了");
+                break;
+            }
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array2 = {5, 2, 7, 4, 1, 3, 8, 9};
+        bubbleEnhance(array2);
+    }
+
+}
---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/BubbleSort.java
@@ -0,0 +1,48 @@
+package org.masteryourself.tutorial.algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>description : BubbleSort, 冒泡排序
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2022/12/9 14:41
+ */
+@Slf4j
+public class BubbleSort {
+
+    public static void bubble(int[] array) {
+        // 一共需要交换 array.length - 1 轮
+        for (int j = 0; j < array.length - 1; j++) {
+            boolean swap = false;
+            // 每一轮冒泡是为了找出最大值, 放到最右边
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                    swap = true;
+                }
+            }
+            log.info("第 {} 轮冒泡, 循环次数是 {}, 冒泡结果是 {}", j + 1, array.length - 1 - j, array);
+            // 如果一次交换都没有发生, 表示数组已经有序了, 不需要再排序了
+            if (!swap) {
+                log.info("数组已经有序了, 不需要再冒泡了");
+                break;
+            }
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = {5, 2, 7, 4, 1, 3, 8, 9};
+        bubble(array1);
+    }
+
+}
