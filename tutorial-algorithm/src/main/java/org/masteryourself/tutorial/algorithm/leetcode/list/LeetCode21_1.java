27e0148093bb7daff21d5001e5501ed76c406f33
masteryourself <renzhao.ruan@foxmail.com>
2023-05-15 11:51:21 +0800

    合并多个升序列表

---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/leetcode/list/LeetCode21_1.java
@@ -0,0 +1,46 @@
+package org.masteryourself.tutorial.algorithm.leetcode.list;
+
+/**
+ * <p>description : LeetCode21_1
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2023/5/15 10:58
+ */
+public class LeetCode21_1 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode sentinel = new ListNode(-1);
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        ListNode p3 = sentinel;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                p3.next = p1;
+                p1 = p1.next;
+            } else {
+                p3.next = p2;
+                p2 = p2.next;
+            }
+            // 新链表也需要向后移动
+            p3 = p3.next;
+        }
+        if (p1 == null) {
+            p3.next = p2;
+        } else {
+            p3.next = p1;
+        }
+        return sentinel.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = ListNode.of(1, 2, 3);
+        ListNode list2 = ListNode.of(1, 2, 4);
+        System.out.println(list1);
+        System.out.println(list2);
+        System.out.println(new LeetCode21_1().mergeTwoLists(list1, list2));
+    }
+
+}
---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/leetcode/list/LeetCode21_2.java
@@ -0,0 +1,38 @@
+package org.masteryourself.tutorial.algorithm.leetcode.list;
+
+/**
+ * <p>description : LeetCode21_2
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2023/5/15 11:26
+ */
+public class LeetCode21_2 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = ListNode.of(1, 2, 3);
+        ListNode list2 = ListNode.of(1, 2, 4);
+        System.out.println(list1);
+        System.out.println(list2);
+        System.out.println(new LeetCode21_2().mergeTwoLists(list1, list2));
+    }
+
+}
