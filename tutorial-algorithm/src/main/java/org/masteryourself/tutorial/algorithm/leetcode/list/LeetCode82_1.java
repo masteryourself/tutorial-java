351c81d2ae01bfb9af6c0e48efb340dbd70e7d78
masteryourself <renzhao.ruan@foxmail.com>
2023-05-15 10:57:59 +0800

    删除排序链表中的所有重复元素

---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/leetcode/list/LeetCode82_1.java
@@ -0,0 +1,41 @@
+package org.masteryourself.tutorial.algorithm.leetcode.list;
+
+/**
+ * <p>description : LeetCode82_1
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2023/5/14 18:12
+ */
+public class LeetCode82_1 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode sentinel = new ListNode(-1, head);
+        ListNode p1 = sentinel;
+        ListNode p2;
+        ListNode p3;
+        while ((p2 = p1.next) != null && (p3 = p2.next) != null) {
+            if (p2.val == p3.val) {
+                while (p3 != null && p2.val == p3.val) {
+                    p3 = p3.next;
+                }
+                p1.next = p3;
+            } else {
+                p1 = p1.next;
+            }
+        }
+        return sentinel.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = ListNode.of(1, 1);
+        System.out.println(head);
+        System.out.println(new LeetCode82_1().deleteDuplicates(head));
+    }
+
+}
---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/leetcode/list/LeetCode82_2.java
@@ -0,0 +1,36 @@
+package org.masteryourself.tutorial.algorithm.leetcode.list;
+
+/**
+ * <p>description : LeetCode82_2
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2023/5/15 10:39
+ */
+public class LeetCode82_2 {
+
+    public ListNode deleteDuplicates(ListNode node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+        if (node.val == node.next.val) {
+            ListNode point = node.next.next;
+            while (point != null && point.val == node.val) {
+                point = point.next;
+            }
+            return deleteDuplicates(point);
+        } else {
+            node.next = deleteDuplicates(node.next);
+            return node;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = ListNode.of(1, 2, 3, 3, 3, 4, 5);
+        System.out.println(head);
+        System.out.println(new LeetCode82_2().deleteDuplicates(head));
+    }
+
+}
