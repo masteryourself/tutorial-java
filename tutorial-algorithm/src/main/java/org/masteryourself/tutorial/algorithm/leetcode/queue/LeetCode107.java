cf3618de520b9425f473e3fd28f49d9b5d6c11d8
masteryourself <renzhao.ruan@foxmail.com>
2023-05-16 16:54:33 +0800

    二叉树遍历

---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/leetcode/queue/LeetCode107.java
@@ -0,0 +1,59 @@
+package org.masteryourself.tutorial.algorithm.leetcode.queue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <p>description : LeetCode107
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2023/5/16 16:12
+ */
+public class LeetCode107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int nextCycle = 1;
+        while (!queue.isEmpty()) {
+            int newNextCycle = 0;
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < nextCycle; i++) {
+                TreeNode tempRoot = queue.poll();
+                if (tempRoot == null) {
+                    continue;
+                }
+                level.add(tempRoot.val);
+                if (tempRoot.left != null) {
+                    queue.offer(tempRoot.left);
+                    newNextCycle++;
+                }
+                if (tempRoot.right != null) {
+                    queue.offer(tempRoot.right);
+                    newNextCycle++;
+                }
+            }
+            nextCycle = newNextCycle;
+            result.addFirst(level);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2, null, null),
+                new TreeNode(3,
+                        new TreeNode(4, null, null),
+                        new TreeNode(5, null, null)));
+        System.out.println(new LeetCode107().levelOrderBottom(root));
+    }
+
+}
---
+++ tutorial-algorithm/src/main/java/org/masteryourself/tutorial/algorithm/leetcode/queue/TreeNode.java
@@ -0,0 +1,31 @@
+package org.masteryourself.tutorial.algorithm.leetcode.queue;
+
+/**
+ * <p>description : TreeNode
+ *
+ * <p>blog : https://www.yuque.com/ruanrenzhao/
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2023/5/16 16:12
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
